e0158a2c6fcc97d4e31c48ac866e05969c3120b5
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-04-30 22:44:43 -0300

    testes para caracteres unicos invalidos

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -13,14 +13,10 @@ TEST_CASE ("Caractere unico e valido inserido","[romanos]") {
     REQUIRE (romanos_para_decimal("M") == 1000);
 }
 
-
-
-
-TEST_CASE( "Numeros romanos - algarismos inv�lidos", "[romanos]" ) {
-    REQUIRE( romanos_para_decimal("G") == -1 );
-    
-    REQUIRE( romanos_para_decimal("i") == -1 );
-
-    
+TEST_CASE ("Caractere unico e invalido (nao romano) inserido","[romanos]") {
+    REQUIRE (romanos_para_decimal("2") == -1);
+    REQUIRE (romanos_para_decimal("2") == -1);
+    REQUIRE (romanos_para_decimal("0") == -1);
+    REQUIRE (romanos_para_decimal("G") == -1);
+    REQUIRE (romanos_para_decimal("H") == -1);
 }
-
