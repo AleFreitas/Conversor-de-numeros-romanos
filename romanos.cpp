aeeec887b401d6adf34987a347e5c88e3309cd23
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-04-30 23:16:30 -0300

    verificação de caracteres implementada

--- romanos.cpp
+++ romanos.cpp
@@ -1,5 +1,29 @@
 #include "romanos.hpp"
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int letra_valida(char letra) {
+  if (letra == 'I' || letra == 'V' || letra == 'X' ||
+    letra == 'L' || letra == 'C' || letra == 'D' ||
+    letra == 'M') return 1;
+  return 0;
+}
+
+int string_valida(string numero) {
+  for (int i = 0; i < numero.length(); i++) {
+    if (!letra_valida(numero[i])) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+
 int romanos_para_decimal(char const * num_romano)
 {
+  if (!string_valida(num_romano)) return -1;
   return 0; 
 }
